6233ab55e1942836e82ec39285da058162a49180
Dabezt <osama.id78@gmail.com>
2021-11-19 18:47:36 +0300

    Changes to RegEntryID and help message: - Now prints help message if the user did not use --attach/-a or --detach/-d - Introduced option to print RegEntryID rather than always printing it - Now lets user know that the disk they specified to detach is dettached

--- Sources/attachdetachsw/main.swift
+++ Sources/attachdetachsw/main.swift
@@ -7,8 +7,9 @@ let doDetach = CMDLineArgs.contains("--detach") || CMDLineArgs.contains("-d")
 let doAttach = CMDLineArgs.contains("--attach") || CMDLineArgs.contains("-a")
 let userWantsHelpMessage = CMDLineArgs.contains("--help") || CMDLineArgs.contains("-h")
 let shouldSetAutoMount = CMDLineArgs.contains("--set-auto-mount")
+let shouldPrintRegEntryID = CMDLineArgs.contains("--get-reg-entry-id") || CMDLineArgs.contains("-g")
 
-if CMDLineArgs.isEmpty || userWantsHelpMessage {
+func printHelp() {
     print("""
           AttachDetachSW --- A Swift recreation of attach-detach.
           
@@ -19,6 +20,7 @@ if CMDLineArgs.isEmpty || userWantsHelpMessage {
           Attach Options:
           --file-mode=FILEMODE              Specify the filemode to attach the specified DMG with
           --set-auto-mount                  Sets the automount to true while attaching specified DMG
+          -g, --get-reg-entry-id            Prints the RegEntryID after attaching DMG
           
           Notes:
           It doesn't make sense to use any Attach Options with --detach / -d
@@ -27,6 +29,9 @@ if CMDLineArgs.isEmpty || userWantsHelpMessage {
           attachdetachsw --attach randomDMG.dmg
           attachdetachsw --detach disk7
           """)
+}
+if CMDLineArgs.isEmpty || userWantsHelpMessage || (!doDetach && !doAttach) {
+    printHelp()
 }
 
 if doDetach {
@@ -57,6 +62,7 @@ if doDetach {
             close(fd)
             fatalError("Error encountered while ejecting \(diskNameToUse): \(String(cString: strerror(errno)))")
         }
+        print("Detached \(diskNameToUse)")
     }
 }
 
@@ -96,8 +102,12 @@ if doAttach {
             fatalError("Attached DMG \"\(dmg)\" However couldn't get name of attached disk.")
         }
         print("Attached as \(bsdName)")
-        if let regEntryID = handler?.regEntryID() {
-            print("regEntryID: \(regEntryID)")
+        if shouldPrintRegEntryID {
+            if let regEntryID = handler?.regEntryID() {
+                print("regEntryID: \(regEntryID)")
+            } else {
+                print("Wasn't able to obtain regEntryID.")
+            }
         }
     }
 }
