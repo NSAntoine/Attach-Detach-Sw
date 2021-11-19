8e38475c5b6d3ebefe1edccc8018a4394aebd507
Dabezt <osama.id78@gmail.com>
2021-11-19 10:52:42 +0300

    working attach

---
+++ Sources/attachdetachsw/Bridge.h
@@ -0,0 +1,36 @@
+#include <Foundation/Foundation.h>
+
+@interface DIBaseParams : NSObject <NSSecureCoding, NSCoding>
+-(id)initWithCoder:(id)arg1 ;
+-(NSURL *)inputURL;
+-(void)encodeWithCoder:(id)arg1 ;
+-(id)description;
+-(id)initWithURL:(id)arg1 fileOpenMode:(unsigned short)arg2 error:(id)arg3 ;
+@end
+
+
+@interface DIAttachParams : DIBaseParams {
+
+    BOOL _autoMount;
+    BOOL _handleRefCount;
+    long long _fileMode;
+}
+@property (assign) BOOL autoMount;                                         //@synthesize autoMount=_autoMount - In the implementation block
+@property (assign,nonatomic) long long fileMode;                           //@synthesize fileMode=_fileMode - In the implementation block
+-(id)initWithURL:(id)arg1 error:(id)arg2 ;
+-(id)initWithCoder:(id)arg1 ;
+-(BOOL)autoMount;
+-(long long)fileMode;
+-(void)setFileMode:(long long)arg1 ;
+-(void)setAutoMount:(BOOL)arg1 ;
+@end
+
+@interface DIDeviceHandle : NSObject
+-(NSString *)BSDName;
+-(unsigned long long)regEntryID;
+@end
+
+@interface DiskImages2 : NSObject
+
++(void)attachWithParams:(DIAttachParams *)param handle:(DIDeviceHandle **)h error:(NSError **)err;
+@end
--- Sources/attachdetachsw/main.swift
+++ Sources/attachdetachsw/main.swift
@@ -1,9 +1,33 @@
 import Foundation
+import DI2Support
 
 let CMDLineArgs = Array(CommandLine.arguments.dropFirst())
 
 let doDetach = CMDLineArgs.contains("--detach") || CMDLineArgs.contains("-d")
 let doAttach = CMDLineArgs.contains("--attach") || CMDLineArgs.contains("-a")
+let userWantsHelpMessage = CMDLineArgs.contains("--help") || CMDLineArgs.contains("-h")
+let shouldSetAutoMount = CMDLineArgs.contains("--set-auto-mount")
+
+if CMDLineArgs.isEmpty || userWantsHelpMessage {
+    print("""
+          AttachDetachSW --- A Swift recreation of attach-detach.
+          
+          General Options:
+          -a, --attach [DMGFILE]            Specify a DMG file to attach
+          -d, --detach [DISKNAME]           Specify a disk name to detach
+          
+          Attach Options:
+          --file-mode=FILEMODE              Specify the filemode to attach the specified DMG with
+          --set-auto-mount                  Sets the automount to true while attaching specified DMG
+          
+          Notes:
+          It doesn't make sense to use any Attach Options with --detach / -d
+          
+          Example usage:
+          attachdetachsw --attach randomDMG.dmg
+          attachdetachsw --detach disk7
+          """)
+}
 
 if doDetach {
     let arrOfDiskPathsSpecified = CMDLineArgs.filter() { $0.contains("disk") }
@@ -13,10 +37,12 @@ if doDetach {
         
         let fd = open(diskNameToUse, O_RDONLY)
         guard fd != -1 else {
+            close(fd)
             fatalError("Error encountered while opening \(diskNameToUse): \(String(cString: strerror(errno)))")
         }
         
         // See more here: https://stackoverflow.com/questions/69961734/getting-ioctl-numbers-in-swit/69961934#69961934
+        
         var ioctlEjectCode:UInt {
             let IOC_VOID: UInt = 0x20000000
             // DKIOEJECT Code is the 21th of group d
@@ -28,7 +54,50 @@ if doDetach {
         }
         let ret = ioctl(fd, ioctlEjectCode)
         guard ret != -1 else {
+            close(fd)
             fatalError("Error encountered while ejecting \(diskNameToUse): \(String(cString: strerror(errno)))")
+        }
+    }
+}
+
+if doAttach {
+    let arrOfSpecifiedDMGs = CMDLineArgs.filter() { NSString(string: $0).pathExtension == "dmg" }
+    var handler: DIDeviceHandle?
+    for dmg in arrOfSpecifiedDMGs {
+        guard FileManager.default.fileExists(atPath: dmg) else {
+            fatalError("File \"\(dmg)\" doesn't exist, there it can't be used.")
+        }
+        let DMGURL = URL(fileURLWithPath: dmg)
+        var err:NSError?
+        let attachParams = DIAttachParams(url: DMGURL, error: err)
+        guard err == nil else {
+            let errToShow = err?.localizedFailureReason ?? err?.localizedDescription
+            fatalError("Error encountered with DIAttachParams: \(errToShow ?? "Unknown Error")")
+        }
+        attachParams?.autoMount = shouldSetAutoMount
+        let fileModeArr = CMDLineArgs.filter() { $0.hasPrefix("--file-mode=") }
+        if !fileModeArr.isEmpty {
+            let fileModeSpecified = fileModeArr[0].replacingOccurrences(of: "--file-mode=", with: "")
+            guard let fileModeSpecifiedInt = Int64(fileModeSpecified) else {
+                fatalError("User used --file-mode however there was either no file mode specified or the filemode specified wasn't an Int. SYNTAX: --file-mode=FILE-MODE, example: --file-mode=2")
+            }
+            attachParams?.fileMode = fileModeSpecifiedInt
+            print("Set file mode to \(fileModeSpecifiedInt)")
+        }
+        
+        DiskImages2.attach(with: attachParams, handle: &handler, error: &err)
+        
+        guard err == nil else {
+            let errToShow = err?.localizedFailureReason ?? err?.localizedDescription
+            fatalError("Error encountered while attaching DMG \"\(dmg)\": \(errToShow ?? "Unknown Error")")
+        }
+        
+        guard let bsdName = handler?.bsdName() else {
+            fatalError("Attached DMG \"\(dmg)\" However couldn't get name of attached disk.")
+        }
+        print("Attached as \(bsdName)")
+        if let regEntryID = handler?.regEntryID() {
+            print("regEntryID: \(regEntryID)")
         }
     }
 }
