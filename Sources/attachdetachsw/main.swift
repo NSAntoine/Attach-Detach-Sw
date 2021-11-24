1a54e46a391c9d5280cc019564bd026931e3918c
Dabezt <osama.id78@gmail.com>
2021-11-24 15:32:05 +0300

    Add documentation in main.swift, fix attaching multiple DMGs

--- Sources/attachdetachsw/main.swift
+++ Sources/attachdetachsw/main.swift
@@ -4,10 +4,12 @@ import DI2Support
 let CMDLineArgs = Array(CommandLine.arguments.dropFirst())
 
 let arrOfSpecifiedDMGs = CMDLineArgs.filter() { NSString(string: $0).pathExtension == "dmg" && FileManager.default.fileExists(atPath: $0) }
-let doAttach = (CMDLineArgs.contains("--attach") || CMDLineArgs.contains("-a")) || !arrOfSpecifiedDMGs.isEmpty // Detect if the user used --attach/-a or if the user specified a DMG without --attach/-a
+// Detect if the user used --attach/-a or if the user specified a DMG without --attach/-a
+let doAttach = (CMDLineArgs.contains("--attach") || CMDLineArgs.contains("-a")) || !arrOfSpecifiedDMGs.isEmpty
 
 let arrOfDiskPathsSpecified = CMDLineArgs.filter() { $0.contains("disk") && NSString(string: $0).pathExtension != "dmg" }
-let doDetach = (CMDLineArgs.contains("--detach") || CMDLineArgs.contains("-d")) || !arrOfDiskPathsSpecified.isEmpty // Detect if the user used --detach/-d or if the user specified a disk without --detach/-d
+// Detect if the user used --detach/-d or if the user specified a disk without --detach/-d
+let doDetach = (CMDLineArgs.contains("--detach") || CMDLineArgs.contains("-d")) || !arrOfDiskPathsSpecified.isEmpty
 
 let userWantsHelpMessage = CMDLineArgs.contains("--help") || CMDLineArgs.contains("-h")
 let shouldPrintRegEntryID = CMDLineArgs.contains("--reg-entry-id") || CMDLineArgs.contains("-r")
@@ -81,7 +83,6 @@ if doAttach {
     guard !arrOfSpecifiedDMGs.isEmpty else {
         fatalError("User used --attach / -a however either didn't specify a DMG file or specified a DMG file that doesn't exist. See attachdetachsw --help for more information.")
     }
-    var handler:DIDeviceHandle?
     
     for dmg in arrOfSpecifiedDMGs {
         let DMGURL = URL(fileURLWithPath: dmg)
@@ -91,8 +92,10 @@ if doAttach {
             let errToShow = attachParamsErr?.localizedFailureReason ?? attachParamsErr?.localizedDescription
             fatalError("Error encountered with DIAttachParams: \(errToShow ?? "Unknown Error")")
         }
+        
         attachParams?.autoMount = CMDLineArgs.contains("--set-auto-mount") || CMDLineArgs.contains("-s")
         
+        // Check if the user used --file-mode or -f correctly
         let fileModeArr = CMDLineArgs.filter() { $0.hasPrefix("--file-mode=") || $0.hasPrefix("-f=") }.map() { $0.replacingOccurrences(of: "--file-mode=", with: "").replacingOccurrences(of: "-f=", with: "")}
         let fileModeArrIntOnly = fileModeArr.compactMap() { Int64($0) }
         if !fileModeArrIntOnly.isEmpty {
@@ -101,8 +104,14 @@ if doAttach {
             attachParams?.fileMode = fileModeToSet
         }
         
+        // Handler which will have the info of the attached DMG
+        var handler:DIDeviceHandle?
+        
+        
+        // If an error occurs while attaching, it'll be set to the following variable
         var attachErr:NSError?
-        DiskImages2.attach(with: attachParams, handle: &handler, error: &attachErr) // Call attach function
+        // Call attach function
+        DiskImages2.attach(with: attachParams, handle: &handler, error: &attachErr)
         
         // Make sure no errors were encountered
         guard attachErr == nil else {
@@ -114,6 +123,7 @@ if doAttach {
             fatalError("Attached DMG However couldn't get info from handler..")
         }
         
+        // Below gets triggered if the user used --verify/-v
         if shouldVerify {
             var verifyErr:NSError?
             let DIVerify = DIVerifyParams(url: URL(fileURLWithPath: "/dev/\(BSDName)"), error: verifyErr)
@@ -131,7 +141,8 @@ if doAttach {
             print("regEntryID: \(handler.regEntryID)")
         }
         
-        let devDiskDirsThatDoExist = ["/dev/\(BSDName)", "/dev/\(BSDName)s1", "/dev/\(BSDName)s1s1"].filter() { FileManager.default.fileExists(atPath: $0) } // Make an array of the devDisk Dirs that should exist, and filter by the ones that actually do
+        // Make an array of the devDisk Dirs that should exist, and filter by the ones that actually do
+        let devDiskDirsThatDoExist = ["/dev/\(BSDName)", "/dev/\(BSDName)s1", "/dev/\(BSDName)s1s1"].filter() { FileManager.default.fileExists(atPath: $0) }
         
         if shouldPrintAllDiskDirs {
             print("All dev disk directories DMG Was attached to: \(devDiskDirsThatDoExist.joined(separator: ", "))")
