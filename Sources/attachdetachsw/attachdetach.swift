2af1bd08a7bc73951d2beef6a3e2083264bcb372
Dabezt <osama.id78@gmail.com>
2021-12-17 13:17:38 +0300

    Overhaul project part 1

--- Sources/attachdetachsw/attachdetach.swift
+++ Sources/attachdetachsw/attachdetach.swift
@@ -1,113 +1,87 @@
-// Contains functions for attaching and detaching
-
 import Foundation
 import DI2Support
 
-func detachDisk(DiskName: inout String) {
-    
-    // If the disk name specified dosesn't start with "/dev/", insert "/dev/" to it at the beginning
-    if !DiskName.hasPrefix("/dev/") {
-        DiskName.insert(contentsOf: "/dev/", at: DiskName.startIndex)
-    }
-    
-    let fd = open(DiskName, O_RDONLY)
-    defer { close(fd) }
-    guard fd != -1 else {
-        let errorEncountered = String(cString: strerror(errno)) // Convert CString strerror to a swift string
-        fatalError("Error encountered while opening \(DiskName): \(errorEncountered)")
-    }
-    
-    // See more here: https://stackoverflow.com/questions/69961734/getting-ioctl-numbers-in-swit/69961934#69961934
-    var ioctlEjectCode:UInt {
-        let IOC_VOID: UInt = 0x20000000
-        // DKIOEJECT Code is the 21th of group d
-        let char = "d" as Character
-        let num = 21 as UInt
-        let g = UInt(UInt(char.asciiValue!) << 8)
-        
-        return IOC_VOID | g | num
-    }
-    let ret = ioctl(fd, ioctlEjectCode)
-    guard ret != -1 else {
-        let errorEncountered = String(cString: strerror(errno)) // Convert CString strerror to a swift string
-        fatalError("Error encountered while ejecting \(DiskName): \(errorEncountered)")
-    }
-    print("Detached \(DiskName)")
+let fm = FileManager.default
+
+func resolveSymlinks(ofPath path: String) -> String {
+    let resolvedPath = try? fm.destinationOfSymbolicLink(atPath: path)
+    // Return the resolved path if we can get ti
+    // otherwise return the original path
+    return resolvedPath ?? path
 }
 
-func attachDMG(DMGFile dmg:String) {
-    let DMGURL = URL(fileURLWithPath: dmg)
-    var attachParamsErr:NSError?
-    let attachParams = DIAttachParams(url: DMGURL, error: attachParamsErr)
+func getIoctlNumber(group: Character, number n:UInt) -> UInt {
+    let void = UInt(IOC_VOID)
+    let g: UInt = UInt(group.asciiValue!) << 8
     
-    attachParams?.autoMount = CMDLineArgs.contains("--set-auto-mount") || CMDLineArgs.contains("-s")
+    return void | g | n
+}
+
+func detachDisk(diskName: inout String) {
+    if !diskName.hasPrefix("/dev/") {
+        diskName.insert(contentsOf: "/dev/", at: diskName.startIndex)
+    }
     
-    // Check if the user used --file-mode or -f correctly
-    let fileModeSpecified = CMDLineArgs.filter {
+    let fd = open(diskName, O_RDONLY)
+    // Just to be safe, close it once done
+    defer { close(fd) }
+    // Make sure no issues were encountered with opening the disk
+    guard fd != -1 else {
+        let errorEncountered = String(cString: strerror(errno))
+        fatalError("Error encountered with opening \(diskName): \(errorEncountered)")
+    }
+    
+    let ejectIOCTLNumber = getIoctlNumber(group: "d", number: 21)
+    
+    let ret = ioctl(fd, ejectIOCTLNumber)
+    guard ret != -1 else {
+        let errorEncountered = String(cString: strerror(errno))
+        fatalError("Error encountered with ejecting \(diskName): \(errorEncountered)")
+    }
+    print("Detached \(diskName)")
+}
+
+func AttachDMG(atPath path: String, completionHandler: (DIDeviceHandle?, Error?) -> Void) {
+    var AttachParamsErr: NSError?
+    let AttachParams = DIAttachParams(url: URL(fileURLWithPath: path), error: AttachParamsErr)
+    if let AttachParamsErr = AttachParamsErr {
+        return completionHandler(nil, AttachParamsErr)
+    }
+    
+    // Set the filemode
+    // if the user didn't specify it by the command line, it'll be 0
+    AttachParams?.fileMode = returnFileModeFromCMDLine()
+    
+    AttachParams?.autoMount = CMDLineArgs.contains("-s") || CMDLineArgs.contains("--set-auto-mount")
+    
+    var handle: DIDeviceHandle?
+    
+    var AttachErr: NSError?
+    let didSuccessfullyAttach = DiskImages2.attach(with: AttachParams, handle: &handle, error: &AttachErr)
+    if let AttachErr = AttachErr {
+        return completionHandler(nil, AttachErr)
+    }
+    
+    return completionHandler(handle, nil)
+}
+
+/// Returns the Attach filemode specified by the user using the `--file-mode=/-f=` options
+func returnFileModeFromCMDLine() -> Int64 {
+    /// The array who's first element (may) be the specified filemode
+    let fileModeArray = CMDLineArgs.filter {
+        // First lets filter the array by the element that contains --file-mode= or -f= in it
         $0.hasPrefix("--file-mode=") || $0.hasPrefix("-f=")
     }.map {
-        // Remove --file-mode=/-f= so we just have the number
-        $0.replacingOccurrences(of: "--file-mode=", with: "").replacingOccurrences(of: "-f=", with: "")
+        // And now lets remove --file-mode=/-f= from the string
+        $0.replacingOccurrences(of: "--file-mode=", with: "")
+            .replacingOccurrences(of: "-f=", with: "")
     }.compactMap {
+        // And now lets allow only Int64 in the array
         Int64($0)
     }
     
-    
-    // If the user did indeed specify a value with --file-mode/-f
-    // set the fileMode to the specified value
-    // otherwise keep it as the default.
-    if !fileModeSpecified.isEmpty {
-        attachParams?.fileMode = fileModeSpecified[0]
+    if fileModeArray.isEmpty {
+        return 0
     }
-    print("Proceeding to attach DMG \(dmg) with filemode \(attachParams?.fileMode ?? 1)")
-    
-    // check if there were any issues encountered with attach parameters
-    if let attachParamsErr = attachParamsErr {
-        print("Error encountered while setting Attach Parameters: \(attachParamsErr.localizedDescription)")
-        print("Recovery Suggestion: \(attachParamsErr.localizedRecoverySuggestion ?? "Not available")")
-        fatalError()
-    }
-    
-    // Handler which will have the info of the attached DMG
-    var handler:DIDeviceHandle?
-    
-    // If an error occurs while attaching, it'll be set to the following variable
-    var attachErr:NSError?
-    
-    // Call attach function
-    let didSuccessfullyAttach = DiskImages2.attach(with: attachParams, handle: &handler, error: &attachErr)
-    
-    // Make sure didSuccessfullyAttach returns true
-    guard didSuccessfullyAttach else {
-        // Print the error if we can
-        if let attachErr = attachErr {
-            print("Error encountered with attaching DMG: \(attachErr.localizedDescription)")
-            print("Recovery Suggestion: \(attachErr.localizedRecoverySuggestion ?? "Not available")")
-        }
-        fatalError("Couldn't successfully attach DMG \(dmg).")
-    }
-    // Get information from handler and make sure the program can get the name of the disk that the DMG was attachedq to
-    guard let handler = handler, let BSDName = handler.bsdName else {
-        fatalError("Attached DMG However couldn't get info from handler..")
-    }
-    
-    print("Attached as \(BSDName)")
-    
-    if shouldPrintRegEntryID {
-        print("\(BSDName) regEntryID: \(handler.regEntryID)")
-    }
-    
-    // Make an array of the devDisk Dirs that should exist, and filter by the ones that actually do
-    let devDiskDirsThatDoExist = ["/dev/\(BSDName)", "/dev/\(BSDName)s1", "/dev/\(BSDName)s1s1"].filter() { FileManager.default.fileExists(atPath: $0) }
-    
-    if shouldPrintAllDiskDirs {
-        print("All dev disk directories DMG Was attached to: \(devDiskDirsThatDoExist.joined(separator: ", "))")
-    }
+    return fileModeArray[0]
 }
-
-func resolveSymlink(ofPath path:String) -> String {
-    let ResolvedPath = try? FileManager.default.destinationOfSymbolicLink(atPath: path)
-    // Return the resolved path if we were able to get it, otherwise return the normal path
-    return ResolvedPath ?? path
-}
-
